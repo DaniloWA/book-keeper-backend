a0b3051b8a0c0a70b1927b2c06da7dde5d21dbdd
Danilo Oliveira <daniloworkdev@gmail.com>
2024-03-17 18:53:38 -0300

    feat: created migrate and model Author

---
+++ app/Models/Author.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'my_flights';
+
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'bio',
+        'avatar'
+    ];
+
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'first_name' => 'string',
+            'last_name' => 'string',
+            'bio' => 'string',
+            'avatar' => 'string',
+        ];
+    }
+}
