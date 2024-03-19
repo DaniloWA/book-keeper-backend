8084f5f45631c5a8d43068961f90c71cdba560c9
jamile matos <jamilematosreis@gmail.com>
2024-03-18 21:52:19 -0300

    feat: add fillable && edit name for username

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'firstname',
+        'lastname',
     ];
 
     /**
@@ -42,6 +44,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'username' => 'string'
         ];
     }
 }
