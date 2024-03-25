b81c63ea5c69356d170f121940d1650b980eebe2
jamile matos <jamilematosreis@gmail.com>
2024-03-25 10:06:49 -0300

    criei o que estava faltando

--- routes/v1/author.php
+++ routes/v1/author.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Author\AuthorController;
 
-Route::post('/author', [AuthorController::class, 'store'])->name('author.store');
-Route::get('/author/{id}', [AuthorController::class, 'show'])->name('author.show');
-Route::put('/author/{id}', [AuthorController::class, 'update'])->name('author.update');
-Route::delete('/author/{id}', [AuthorController::class, 'destroy'])->name('author.destroy');
+Route::post('/authors', [AuthorController::class, 'store'])->name('author.store');
+Route::get('/authors/{id}', [AuthorController::class, 'show'])->name('author.show');
+Route::put('/authors/{id}', [AuthorController::class, 'update'])->name('author.update');
+Route::delete('/authors/{id}', [AuthorController::class, 'destroy'])->name('author.destroy');
