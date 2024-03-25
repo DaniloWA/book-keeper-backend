bcaa1fc45b9c51d0161d5b8e254d98e165adc272
skapskap <skaprange@gmail.com>
2024-03-25 10:41:08 -0300

    fix: update book routes names

--- routes/v1/book.php
+++ routes/v1/book.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Book\BookController;
 
-Route::post('/books', [BookController::class, 'store'])->name('book.store');
-Route::get('/books', [BookController::class, 'index'])->name('book.show');
-Route::get('/books/{uuid}', [BookController::class, 'show'])->name('book.show');
-Route::put('/books/{uuid}', [BookController::class, 'update'])->name('book.update');
-Route::delete('/books/{uuid}', [BookController::class, 'destroy'])->name('book.destroy');
+Route::post('/books', [BookController::class, 'store'])->name('store');
+Route::get('/books', [BookController::class, 'index'])->name('show');
+Route::get('/books/{uuid}', [BookController::class, 'show'])->name('show');
+Route::put('/books/{uuid}', [BookController::class, 'update'])->name('update');
+Route::delete('/books/{uuid}', [BookController::class, 'destroy'])->name('destroy');
