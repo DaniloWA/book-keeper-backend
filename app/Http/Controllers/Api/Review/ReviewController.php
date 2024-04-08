7de356a12a98780ccbb7c06a488b48478b8bb53a
jamile matos <jamilematosreis@gmail.com>
2024-04-08 18:14:37 -0300

    author and review refactoring with trait were created

--- app/Http/Controllers/Api/Review/ReviewController.php
+++ app/Http/Controllers/Api/Review/ReviewController.php
@@ -6,74 +6,57 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ReviewRequest;
 use App\Models\Book;
 use App\Models\Review;
+use App\Traits\ApiResponser;
 use Illuminate\Support\Facades\Auth;
 
-class ReviewController extends Controller
-{
+class ReviewController extends Controller{
+    use ApiResponser;
     public function index()
     {
         $reviews = Review::all();
-        return response()->json($reviews, 200);
+        return $this->successResponse($reviews, 200);
     }
-    /**
-        *
-        *
-
-        * @return \Illuminate\Http\Response
-        */
 
     public function store(ReviewRequest $request)
     {
         $validatedData = $request->validated();
-
         $book = Book::where('uuid', $validatedData['book_uuid'])->first();
-
         $validatedData['book_id'] = $book->id;
         $validatedData['user_id'] = Auth::id();
         $review = Review::create($validatedData);
 
-        return response()->json($review, 201);
+        return $this->successResponse($review, 201);
     }
-
 
     public function show($id)
     {
         $review = Review::where('id', $id)-> first();
-
         if ($review) {
-            return response()->json($review, 200);
+            return $this->successResponse($review, 200);
         } else {
-            return response()->json(['error' => 'Review not found'], 404);
+            return $this->errorResponse('Review not found', 404);
         }
     }
-
-
 
     public function update(ReviewRequest $request, $id)
     {
         $review = Review::where('id', $id)->first();
-
         if ($review) {
             $review->update($request->validated());
-            return response()->json($review, 200);
+            return $this->successResponse($review,200);
         } else {
-            return response()->json(['error' => 'Review not found'], 404);
+            return $this->errorResponse('Review not found', 404);
         }
     }
-
 
     public function destroy($id)
     {
         $review  = Review::where('id', $id)->first();
-
         if ($review) {
-
             $review->delete();
-
-            return response()->json(['message' => 'Review  deleted successfully'], 200);
+            return $this->successResponse(['message' => 'Review deleted successfully'], 200);
         } else {
-            return response()->json(['error' => 'Review not found'], 404);
+            return $this->errorResponse('Review not found', 404);
         }
-
     }
 }
