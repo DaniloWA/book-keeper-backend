1bcae8e578f76272a3f5cfacc02ea69e2eb360e0
jamile matos <jamilematosreis@gmail.com>
2024-04-10 08:20:44 -0300

    creation of the review factory and seed

---
+++ database/seeders/ReviewSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Review;
+
+class ReviewSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Review::factory()->count(10)->create();
+    }
+}
