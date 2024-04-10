743c896d50c2825c8e40f19072894d302ac3ec5c
skapskap <skaprange@gmail.com>
2024-04-10 14:58:34 -0300

    refactor: usage of profile states inside of profileseeder and making the code more efficient

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Profile;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProfileSeeder extends Seeder
@@ -14,18 +13,32 @@ class ProfileSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = User::all();
+        // EFFICIENT WAY
 
-        if ($users->isEmpty()) {
-            $users = User::factory()->count(10)->create();
-        }
+        if (User::count() === 0) {
+            User::factory()->count(10)->withProfile()->create();
+        } else {
+            $userIdsWithProfile = User::has('profile')->pluck('id');
+            $usersWithoutProfile = User::whereNotIn('id', $userIdsWithProfile)->get();
 
-        foreach ($users as $user) {
-            $profile = Profile::where('user_id', $user->id)->exists();
-
-            if (!$profile) {
+            foreach ($usersWithoutProfile as $user) {
                 Profile::factory()->setUser($user)->create();
             }
         }
+
+        // LESS EFFICIENT WAY
+        // $users = User::all();
+
+        // if ($users->isEmpty()) {
+        //     $users = User::factory()->count(10)->create();
+        // }
+
+        // foreach ($users as $user) {
+        //     $profile = Profile::where('user_id', $user->id)->exists();
+
+        //     if (!$profile) {
+        //         Profile::factory()->setUser($user)->create();
+        //     }
+        // }
     }
 }
