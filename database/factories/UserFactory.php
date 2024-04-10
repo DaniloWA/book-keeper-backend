b2c77dc8f21a7a10b62e6f957f14bb2c04132658
skapskap <skaprange@gmail.com>
2024-04-10 14:57:00 -0300

    feat: implement user factory state related to profiles

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Profile;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -42,5 +43,10 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function withProfile(): static
+    {
+        return $this->has(Profile::factory(), 'profile');
     }
 }
